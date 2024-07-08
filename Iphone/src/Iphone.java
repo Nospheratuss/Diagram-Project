0c3c3bc50abe430c9168f03440f1907331830327
Nospheratuss <157959142+Nospheratuss@users.noreply.github.com>
2024-07-08 02:58:39 -0300

    Add files via upload

---
+++ Iphone/src/Iphone.java
@@ -0,0 +1,68 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+    
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+       
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+       
+        meuIphone.exibirPagina("http://www.dio.me");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
